d1059970caaf932ac1c136a4a84df5dd8e228144
Miller Cy Chan <miller.chan@gmail.com>
2025-02-26 10:22:13 +0800

    Update PnnLABQuantizer.cpp

--- nQuantGpp/PnnLABQuantizer.cpp
+++ nQuantGpp/PnnLABQuantizer.cpp
@@ -401,7 +401,7 @@ namespace PnnLABQuant
 					curdist += sqr(c2[3] - c[3]);
 				}
 			}
-			else if (hasSemiTransparency) {
+			else if (hasSemiTransparency || nMaxColors < 16) {
 				curdist += sqr(lab2.L - lab1.L);
 				if (curdist > mindist)
 					continue;
