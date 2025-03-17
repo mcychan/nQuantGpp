ef24ba74fd59e1019927f6ef9833c868633d11b6
Miller Cy Chan <miller.chan@gmail.com>
2025-03-17 22:00:00 +0800

    Add files via upload

--- nQuantGpp/GilbertCurve.cpp
+++ nQuantGpp/GilbertCurve.cpp
@@ -140,13 +140,13 @@ namespace Peano
 			}
 			
 			if (nMaxColors < 3 || margin > 6) {
-				if (nMaxColors > 8 && (CIELABConvertor::Y_Diff(pixel, c2) > (beta * acceptedDiff) || CIELABConvertor::U_Diff(pixel, c2) > (2 * acceptedDiff))) {
+				if (nMaxColors > 4 && (CIELABConvertor::Y_Diff(pixel, c2) > (beta * acceptedDiff) || CIELABConvertor::U_Diff(pixel, c2) > (2 * acceptedDiff))) {
 					auto kappa = m_saliencies[bidx] < .4f ? beta * .4f * m_saliencies[bidx] : beta * .4f / m_saliencies[bidx];
 					Vec4b c1(b_pix, g_pix, r_pix, a_pix);
 					c2 = BlueNoise::diffuse(c1, qPixel, kappa, strength, x, y);
 				}
 			}
-			else if (nMaxColors > 8 && (CIELABConvertor::Y_Diff(pixel, c2) > (beta * acceptedDiff) || CIELABConvertor::U_Diff(pixel, c2) > acceptedDiff)) {
+			else if (nMaxColors > 4 && (CIELABConvertor::Y_Diff(pixel, c2) > (beta * acceptedDiff) || CIELABConvertor::U_Diff(pixel, c2) > acceptedDiff)) {
 				if (beta < .3f && (nMaxColors <= 32 || m_saliencies[bidx] < beta))
 					c2 = BlueNoise::diffuse(c2, qPixel, beta * .4f * m_saliencies[bidx], strength, x, y);
 				else {
@@ -304,7 +304,7 @@ namespace Peano
 		if (nMaxColors > 4) {
 			auto boundary = .005 - .0000625 * nMaxColors;
 			beta = (float) (weight > boundary ? max(.25, beta - nMaxColors * weight) : min(1.5, beta + nMaxColors * weight));
-			if(nMaxColors > 32)
+			if(nMaxColors > 32 && nMaxColors < 256)
 				beta += .1f;
 		}
 		else
