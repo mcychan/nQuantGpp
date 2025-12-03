137d9e8948bd9f40a693d810bc3d9f6cf039db00
Miller Cy Chan <miller.chan@gmail.com>
2025-12-03 11:17:30 +0800

    Add files via upload

--- nQuantGpp/PnnLABGAQuantizer.cpp
+++ nQuantGpp/PnnLABGAQuantizer.cpp
@@ -123,7 +123,7 @@ namespace PnnLABQuant
 		auto errors = _objectives;
 		fill(errors.begin(), errors.end(), 0);
 
-		int threshold = (maxRatio < .1 || _nMaxColors < 32) ? -64 : -112;
+		int threshold = (maxRatio < .1 || abs(_ratioX - _ratioY) > .01) ? -64 : -112;
 
 		int pixelIndex = 0;
 		for (auto& pixels : m_pixelsList) {
